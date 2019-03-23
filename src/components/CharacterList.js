782351b9ffe8b6a914604ffbceb69180d60a00a0
Giang Nguyen <thuygiang152@gmail.com>
2019-03-23 19:14:33 +0200

    map state to props

---
+++ src/components/CharacterList.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+
+class CharacterList extends Component {
+  render() {
+    console.log(this.props);
+    return (
+      <div>
+        <h4>Characters</h4>
+        <ul>
+          {this.props.characters.map(character => {
+            return (
+              <li key={character.id}>
+                <div>{character.name}</div>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  console.log(state);
+  return { characters: state.characters };
+}
+
+export default connect(
+  mapStateToProps,
+  null
+)(CharacterList);
